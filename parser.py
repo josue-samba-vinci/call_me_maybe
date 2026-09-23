197086f95e8da2df6148d9960332fc34c328e990
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-09-23 19:15:01 +0200

    fix on duplicates and order of the function

--- parser.py
+++ parser.py
@@ -2,40 +2,38 @@ import sys
 
 
 def arg_parser() -> dict:
-    #print(sys.argv)
+    print(sys.argv)
     relation: dict = {}
     relation = {"--functions_definition": 
                 "data/input/functions_definition.json",
                 "--input": "data/input/function_calling_tests.json",
                 "--output": "data/output/function_calls.json"}
-    #print(relation)
-    if len(sys.argv) > 2:
+    print(relation)
+    if len(sys.argv) > 1:
         contained: list[str] = []
-        i: int = 2
+        i: int = 1
         while (i < len(sys.argv)):
-            if sys.argv[i] in contained:
-                print("please put each flag only once")
             if not flag(sys.argv[i]):
                 print(f"{sys.argv[i]} is not an accepted flag")
-                print("Please put the flags after uv run python3 -m src")
-                print("--functions_definition <function_definition_file>")
-                print("--input <input_file>")
-                print("--output <output_file>")
-        
-            else:
-                i += 1
-                try:
-                    if not flag(sys.argv[i]):
-                        relation[sys.argv[i-1]] = sys.argv[i]
-                    else:
-                        print("one of your values is a flag")
-                        return None
-                except IndexError:
-                    print("At least one flag doesn't have a value attached to it")
+                return None
+            if sys.argv[i] in contained:
+                print(f"{sys.argv[i]} is a duplicate")
+                return None
+            try:
+                if flag(sys.argv[i+1]):
+                    print("Please put the flags after uv run python3 -m src")
+                    print("--functions_definition <function_definition_file>")
+                    print("--input <input_file>")
+                    print("--output <output_file>")
                     return None
-                contained.append(sys.argv[i-1])
-                i += 1
-    #print(relation)
+                else:
+                    relation[sys.argv[i]] = sys.argv[i+1]
+            except IndexError:
+                print("At least one flag doesn't have a value attached to it")
+                return None
+            contained.append(sys.argv[i])
+            i += 2
+    print(relation)
     return relation
 
 
@@ -46,7 +44,3 @@ def flag(arg: str) -> bool:
     if arg not in flags:
         return False
     return True
-
-
-if __name__ == "__main__":
-    arg_parser()
